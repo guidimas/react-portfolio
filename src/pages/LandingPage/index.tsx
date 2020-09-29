9234d795d98905d5affa5c00d3fb4f4ab95d5c75
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-09-28 21:05:24 -0300

    React Router DOM and Styled Components added to project. Routes configured.

---
+++ src/pages/LandingPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const LandingPage: React.FC = () => {
+  return (
+    <Container>
+      <span>Hello, world!</span>
+    </Container>
+  );
+};
+
+export default LandingPage;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import LandingPage from '../pages/LandingPage';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" component={LandingPage} exact />
+    <Redirect to="/" />
+  </Switch>
+);
+
+export default Routes;
