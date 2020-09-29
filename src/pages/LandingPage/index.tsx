aae1126baea16ba0bf622cd1025b5a96fc68f300
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-09-29 20:00:47 -0300

    Landing page created.

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import Header from '../../components/Header';
 
-import { Container } from './styles';
+import {
+  Container,
+  Main,
+  Left,
+  Right,
+  Title,
+  Description,
+  Button,
+} from './styles';
 
 const LandingPage: React.FC = () => {
   return (
     <Container>
-      <span>Hello, world!</span>
+      <Header />
+      <Main>
+        <Left>
+          <Title>Freelance Developer</Title>
+          <Description>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            incidunt placeat iure exercitationem commodi dignissimos at quaerat
+            quasi suscipit pariatur.
+          </Description>
+          <Button>See projects</Button>
+        </Left>
+        <Right>Right</Right>
+      </Main>
     </Container>
   );
 };
--- src/pages/LandingPage/styles.ts
+++ src/pages/LandingPage/styles.ts
@@ -1,3 +1,47 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #3f3b51;
+`;
+
+export const Main = styled.main`
+  max-width: 1080px;
+  margin: 0 auto;
+`;
+
+/* Left */
+export const Left = styled.section``;
+
+export const Title = styled.h2`
+  font-size: 56px;
+  color: #574e65;
+  line-height: 56px;
+  font-weight: 700px;
+`;
+
+export const Description = styled.div`
+  color: #aba6b2;
+  font-size: 18px;
+  line-height: 18px;
+  margin-top: 2em;
+`;
+
+export const Button = styled.button`
+  border: 0;
+  background: #78d0d3;
+  margin-top: 3em;
+  border-radius: 9px;
+  text-transform: uppercase;
+  padding: 1em 1.5em;
+  box-shadow: 0px 20px 25px -15px rgba(100, 177, 180, 0.8);
+  transition: all 0.2s linear;
+  color: white;
+
+  &:hover {
+    background: #78c9cc;
+    box-shadow: 0px 26px 25px -21px rgba(100, 177, 180, 0.8);
+  }
+`;
+
+/* Right */
+export const Right = styled.section``;
