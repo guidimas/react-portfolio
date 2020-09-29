b6e4a369928e33e13af518a991895e0581361f15
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-09-29 20:00:17 -0300

    Global style created.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes/routes';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
   <Router>
     <Routes />
+    <GlobalStyle />
   </Router>
 );
 
---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea, span, strong {
+    font-family: 'Poppins', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: bold;
+  }
+
+  button {
+    cursor: pointer;
+    outline: 0;
+  }
+
+  ::-moz-selection {
+    background: #fcd661;
+    color: #000;
+  }
+
+  ::selection {
+    background: #fcd661;
+    color: #000;
+  }
+`;
