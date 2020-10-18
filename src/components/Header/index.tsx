fa0d9c90e1315ee6e2891631260bca0859ac95e8
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-10-18 18:14:01 -0300

    Header component updated, logo image added.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
+import { FiMenu } from 'react-icons/fi';
 import { Container, Logo, Navigation, NavigationItem } from './styles';
+
+import logo from '../../assets/logo.svg';
 
 const Header: React.FC = () => {
   return (
     <Container>
-      <Logo>Dimas Portfolio</Logo>
+      <Logo>
+        <img src={logo} alt="Dimas Portfolio" />
+        <h1>Dimas Portfolio</h1>
+      </Logo>
       <Navigation>
         <NavigationItem selected>Developer</NavigationItem>
         <NavigationItem>Designer</NavigationItem>
         <NavigationItem>Manager</NavigationItem>
       </Navigation>
+      <FiMenu size={24} />
     </Container>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,16 +7,47 @@ interface INavigationItemProps {
 export const Container = styled.header`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  width: 100%;
 
-  max-width: 1080px;
   margin: 0 auto;
-  padding: 1em 0;
+  padding: 1em 2em;
+
+  svg {
+    color: #574e65;
+    display: none;
+    cursor: pointer;
+
+    @media (max-width: 640px) {
+      display: block;
+    }
+  }
 `;
 
-export const Logo = styled.h1`
-  color: #574e65;
-  font-size: 24px;
-  font-weight: 500;
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+
+  h1 {
+    color: #574e65;
+    font-size: 24px;
+    font-weight: 500;
+    margin-left: 0.3em;
+  }
+
+  img {
+    width: 30px;
+    height: 30px;
+    transition: opacity 0.2s linear, transform 0.4s ease-out;
+  }
+
+  &:hover {
+    opacity: 0.8;
+
+    img {
+      transform: rotateX(180deg) rotateY(180deg);
+    }
+  }
 `;
 
 export const Navigation = styled.nav`
@@ -25,11 +56,20 @@ export const Navigation = styled.nav`
   button + button {
     margin-left: 2em;
   }
+
+  @media (max-width: 640px) {
+    display: none;
+  }
 `;
 
 export const NavigationItem = styled.button<INavigationItemProps>`
   border: 0;
   background: none;
+  transition: opacity 0.2s linear;
+
+  &:hover {
+    opacity: 0.7;
+  }
 
   ${({ selected }) =>
     selected
