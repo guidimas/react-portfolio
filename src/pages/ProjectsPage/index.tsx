965e5274c733b8e3e56aa4da395a1aa134e3d0a7
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-11-11 19:27:39 -0300

    Repositories hook used on ProjectsPage.

--- src/pages/ProjectsPage/index.tsx
+++ src/pages/ProjectsPage/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRepositories } from '../../hooks/repositories';
 
 import { Container } from './styles';
 
 const ProjectsPage: React.FC = () => {
+  const { loadRepositories, repositories } = useRepositories();
+
+  useEffect(() => {
+    loadRepositories();
+  }, [loadRepositories]);
+
+  useEffect(() => {
+    console.log(repositories);
+  }, [repositories]);
+
   return (
     <Container>
       <h1>ProjectsPage</h1>
