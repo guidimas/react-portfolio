986dbf666789626f296983afe040ab540fc109a1
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-11-10 21:41:15 -0300

    ProjectsPage created. Routes updated.

---
+++ src/pages/ProjectsPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ProjectsPage: React.FC = () => {
+  return (
+    <Container>
+      <h1>ProjectsPage</h1>
+    </Container>
+  );
+};
+
+export default ProjectsPage;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import LandingPage from '../pages/LandingPage';
+import ProjectsPage from '../pages/ProjectsPage';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" component={LandingPage} exact />
+    <Route path="/projects" component={ProjectsPage} exact />
     <Redirect to="/" />
   </Switch>
 );
