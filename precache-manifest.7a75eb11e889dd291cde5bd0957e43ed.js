33250e4eed664bb9914f3414b40b76cf33b862d8
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-11-12 21:07:46 -0300

    Updates

---
+++ precache-manifest.7a75eb11e889dd291cde5bd0957e43ed.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c675d4cd8b98ffbae0fd5e85cb9762c8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3d94037b6cc23f20eaf7",
+    "url": "/react-portfolio/static/js/2.2f9331f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.2f9331f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28455030b010ecd9bf61",
+    "url": "/react-portfolio/static/js/main.215627cd.chunk.js"
+  },
+  {
+    "revision": "be0d8492d3ee3a40a057",
+    "url": "/react-portfolio/static/js/runtime-main.b6fca9e9.js"
+  },
+  {
+    "revision": "0b39699c396879b6d49962f2df4a9b07",
+    "url": "/react-portfolio/static/media/bottom-left.0b39699c.svg"
+  },
+  {
+    "revision": "2e5c5dcc78f227659b5f1f07db137262",
+    "url": "/react-portfolio/static/media/bottom-right.2e5c5dcc.svg"
+  },
+  {
+    "revision": "f95c13370cf5d0ec1a03903b3358b4ca",
+    "url": "/react-portfolio/static/media/ellipse.f95c1337.svg"
+  },
+  {
+    "revision": "a51ee464fae3dca3cfea4caf7033f021",
+    "url": "/react-portfolio/static/media/illustration.a51ee464.svg"
+  },
+  {
+    "revision": "c6483ef562e468a4d1bc9168f11e9390",
+    "url": "/react-portfolio/static/media/logo.c6483ef5.svg"
+  },
+  {
+    "revision": "2ef20f48d3f35a6b047ead73be6eea42",
+    "url": "/react-portfolio/static/media/top-right.2ef20f48.svg"
+  }
+]);
